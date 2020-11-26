7713d6f9b1215fa004a211b53c53b313c2ecc022
Emek Cohen <emek1910@gmail.com>
2020-11-26 14:28:34 +0200

    Added OTA support

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,17 @@
 #include <Arduino.h>
+// ota updates
+#include <ESP8266WiFi.h>
+#include <ESP8266mDNS.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+// wifi
+#define OTA_SSID "Cohensoon"
+#define OTA_TOKEN "0506966260"
+
+void initWiFi();
+void handleOTA();
+
 // relay
 #define RELAY_PIN 5 //D1
 void initRelay();
@@ -18,12 +31,18 @@ unsigned oldMillis = 0;
 #define TIME_OFFSET 10000 // 10 sec
 
 void setup() {
+  Serial.begin(115200);
+
   initSensor();
 
   initRelay();
+
+  initWiFi();
 }
 
 void loop() {
+  handleOTA();
+  
   pirResult = digitalRead(SENSOR_PIN);
   if (pirResult == HIGH) {
     oldMillis = millis();
@@ -52,3 +71,52 @@ void turnOnRelay() {
 void turnOffRelay() {
   digitalWrite(RELAY_PIN, HIGH);
 }
+
+// OTA STUFF
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(OTA_SSID, OTA_TOKEN);
+
+  while (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.println("Connection Failed! Rebooting...");
+    delay(5000);
+    ESP.restart();
+  }
+
+  ArduinoOTA.setPort(8266);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH)
+      type = "sketch";
+    else // U_SPIFFS
+      type = "filesystem";
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
+    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
+    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
+    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
+    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+  });
+
+  ArduinoOTA.begin();
+  Serial.println("Ready");
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP());
+}
+
+void handleOTA() {
+  ArduinoOTA.handle();
+}
+
