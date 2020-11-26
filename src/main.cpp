bd4fdcdb8c72dc0019c7671ce7f9689aeaba8346
Emek Cohen <emek1910@gmail.com>
2020-11-26 16:00:27 +0200

    platformio bugs
    need to give the local address of the chip in platformio.ini file

--- src/main.cpp
+++ src/main.cpp
@@ -28,7 +28,7 @@ void onSensorDetect();
 unsigned oldMillis = 0;
 
 #define LIGHT_ON_TIME 3 * 60000
-#define TIME_OFFSET 10000 // 10 sec
+#define TIME_OFFSET 60000 // 1 min sec
 
 void setup() {
   Serial.begin(115200);
@@ -83,8 +83,6 @@ void initWiFi() {
     ESP.restart();
   }
 
-  ArduinoOTA.setPort(8266);
-
   ArduinoOTA.onStart([]() {
     String type;
     if (ArduinoOTA.getCommand() == U_FLASH)
@@ -95,25 +93,15 @@ void initWiFi() {
     // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
     Serial.println("Start updating " + type);
   });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
   ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
     Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
   });
   ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) Serial.println("Auth Failed");
-    else if (error == OTA_BEGIN_ERROR) Serial.println("Begin Failed");
-    else if (error == OTA_CONNECT_ERROR) Serial.println("Connect Failed");
-    else if (error == OTA_RECEIVE_ERROR) Serial.println("Receive Failed");
-    else if (error == OTA_END_ERROR) Serial.println("End Failed");
+    ESP.restart();
   });
 
   ArduinoOTA.begin();
   Serial.println("Ready");
-  Serial.print("IP address: ");
-  Serial.println(WiFi.localIP());
 }
 
 void handleOTA() {
